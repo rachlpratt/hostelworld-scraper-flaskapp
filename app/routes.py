db64af404295dc1bf765fa59f90e622e81f22486
Rachel Pratt <rachelpratt@RachelsMac.local>
2023-01-25 17:34:08 -0500

    Add about & destinations pages

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,13 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+
+@app.route('/destinations')
+def destinations():
+    return render_template('destinations.html', title='Destinations')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
