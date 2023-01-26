773544e1de635d6f1d1cad146164f01a70958c05
Rachel Pratt <rachelpratt@RachelsMac.local>
2023-01-26 14:38:46 -0500

    Add table of dummy data to random page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from random import choice
 
 
 @app.route('/')
@@ -20,7 +21,24 @@ def about():
 
 @app.route('/random')
 def random():
-    return render_template('random.html', title='Random Destination')
+
+    # Simulate generating a random destination (microservice will do this later)
+    rand_dests = [["asia", "japan", "tokyo"], ["europe", "france", "paris"], ["europe", "italy", "milan"]]
+    rand_dest = choice(rand_dests)
+    country, city = rand_dest[1], rand_dest[2]
+
+    # Create fake hostel names and prices for now - will scrape this info later
+    hostels = [["Hostel 1", 45], ["Hostel 2", 44], ["Hostel 3", 39], ["Hostel 4", 48], ["Hostel 5", 52],
+               ["Hostel 6", 38], ["Hostel 7", 46], ["Hostel 8", 47], ["Hostel 9", 45], ["Hostel 10", 44]]
+
+    # Get average hostel price
+    sum_prices = 0
+    for hostel in hostels:
+        sum_prices += hostel[1]
+    avg_price = round(sum_prices / len(hostels))
+
+    return render_template('random.html', title='Random Destination', hostels=hostels, country=country, city=city,
+                           avg_price=avg_price)
 
 
 @app.route('/destination')
