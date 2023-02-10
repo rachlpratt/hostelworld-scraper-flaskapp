54129e68bbc2726f9e4c3d110051fc6868124271
Rachel Pratt <rachelpratt@RachelsMac.local>
2023-02-10 15:19:50 -0500

    Add web scraping functionality for random page

---
+++ app/helpers.py
@@ -0,0 +1,144 @@
+from random import choice
+import pycountry_convert as pc
+from bs4 import BeautifulSoup
+import requests
+
+
+def find_uk_nation(city):
+    # Return proper UK nation for HostelWorld URL
+    nations = {"england": ["bath", "birmingham", "brighton", "bristol", "liverpool", "london", "manchester", "york"],
+               "scotland": ["edinburgh", "fort william", "glasgow", "inverness", "isle of skye"],
+               "wales": ["cardiff", "caernarfon", "swansea"],
+               "northern ireland": ["belfast", "ballycastle"]}
+    for key in nations.keys():
+        if city in nations[key]:
+            return key
+
+
+def find_continent_of_special_country(country):
+    # Return proper continent for HostelWorld URL
+    countries_with_continents = {
+        "Turkey": "europe",
+        "Aruba": "south-america",
+        "Martinique": "south-america",
+        "Puerto Rico": "south-america",
+        "Usa": "north-america"
+    }
+    if country in countries_with_continents:
+        return countries_with_continents[country]
+
+
+def rename_special_city(city):
+    # Rename city for HostelWorld URL
+    cities_renamed = {
+        "new york city": "new-york"
+    }
+    return cities_renamed[city]
+
+
+def rename_special_country(country):
+    # Rename country for HostelWorld URL
+    countries_renamed = {
+        "Hong Kong": "hong-kong-china",
+        "Taiwan": "taiwan-china"
+    }
+    return countries_renamed[country]
+
+
+def generate_random_destination():
+    # Simulating generating a random city & country pair (will be done by microservice)
+    destinations = [["tokyo", "japan"], ["paris", "france"], ["milan", "italy"], ["sydney", "australia"],
+                    ["new york city", "usa"], ["san francisco", "usa"], ["istanbul", "turkey"], ["athens", "greece"],
+                    ["hong kong", "hong kong"], ["taipei", "taiwan"], ["san juan", "puerto rico"],
+                    ["glasgow", "united kingdom"], ["dublin", "ireland"], ["auckland", "new zealand"]]
+    return choice(destinations)
+
+
+def get_continent(country):
+    # Get continent for countries that have unexpected continent on HostelWorld
+    special_countries = ["Turkey", "Aruba", "Martinique", "Puerto Rico", "Usa", "US Virgin Islands"]
+    if country in special_countries:
+        continent = find_continent_of_special_country(country)
+
+    # Get continent for countries with pycountry
+    else:
+        country_alpha2 = pc.country_name_to_country_alpha2(country, cn_name_format="default")
+        continent_code = pc.country_alpha2_to_continent_code(country_alpha2)
+        continent = pc.convert_continent_code_to_continent_name(continent_code)
+    return continent
+
+
+def format_url(city, country, continent):
+    # For cities located in the UK, find specific nation (HostelWorld uses this in place of UK)
+    if country == "United Kingdom":
+        country = find_uk_nation(city)
+
+    # Rename cities that have different names on HostelWorld
+    special_cities = ["new york city"]
+    if city in special_cities:
+        city = rename_special_city(city)
+
+    # Rename countries that have different names on HostelWorld
+    special_countries = ["Hong Kong", "Taiwan"]
+    if country in special_countries:
+        country = rename_special_country(country)
+
+    # Replace spaces with dashes and ensure everything is lowercase
+    continent = continent.lower().replace(" ", "-")
+    country = country.lower().replace(" ", "-")
+    city = city.replace(" ", "-")
+
+    # Return URL
+    return f"https://www.hostelworld.com/st/hostels/{continent}/{country}/{city}/"
+
+
+def convert_eur_to_usd(price):
+    # Convert price in EUR to USD
+    conversion_rate = 1.0673
+    price = float(price)
+    price = int(price * conversion_rate)
+    return price
+
+
+def get_soup(url):
+    # Gather data from page and parse
+    response = requests.get(url)
+    web_page = response.text
+    return BeautifulSoup(web_page, "html.parser")
+
+
+def get_hostels(soup):
+    # Get first 10 properties on page, excluding featured properties
+    featured_list = soup.find(class_="featured-list")
+    if not featured_list:
+        start_index = 0                     # Will start from first hostel on page if no featured properties
+    else:
+        featured_properties = []
+        featured_list = featured_list.find_all(class_="property-card")
+        for featured_property in featured_list:
+            featured_properties.append(featured_property)
+        start_index = len(featured_properties)
+    hostels = soup.find_all(class_="property")
+    return hostels[start_index:start_index + 10]
+
+
+def get_hostel_names_and_prices(properties):
+    # Get all prices for properties
+    hostel_names_and_prices = []
+    for hostel in properties[:10]:
+        hostel_name_and_price = []
+        title = hostel.find(class_="details").find(class_="title").find("a").text
+        hostel_name_and_price.append(title)
+        price = hostel.find(class_="price").string.strip()
+        price = int(price[1:])
+        hostel_name_and_price.append(convert_eur_to_usd(price))
+        hostel_names_and_prices.append(hostel_name_and_price)
+    return hostel_names_and_prices
+
+
+def get_price_average(hostels):
+    # Get average of list of prices
+    prices = []
+    for hostel in hostels:
+        prices.append(hostel[1])
+    return round(sum(prices) / len(prices))
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template, request
-from random import choice
 from app.forms import SelectDestinationForm
+from app.helpers import *
 
 
 @app.route('/')
@@ -31,21 +31,21 @@ def about():
 def random():
 
     # Simulate generating a random destination (microservice will do this later)
-    rand_dests = [["asia", "japan", "tokyo"], ["europe", "france", "paris"], ["europe", "italy", "milan"],
-                  ["north-america", "canada", "vancouver"], ["oceania", "australia", "sydney"],
-                  ["europe", "turkey", "istanbul"]]
-    rand_dest = choice(rand_dests)
-    country, city = rand_dest[1], rand_dest[2]
+    random_destination = generate_random_destination()
 
-    # Create fake hostel names and prices for now - will scrape this info later
-    hostels = [["Hostel 1", 45], ["Hostel 2", 44], ["Hostel 3", 39], ["Hostel 4", 48], ["Hostel 5", 52],
-               ["Hostel 6", 38], ["Hostel 7", 46], ["Hostel 8", 47], ["Hostel 9", 45], ["Hostel 10", 44]]
+    # Obtain city, country, and continent of random destination
+    city = random_destination[0]
+    country = random_destination[1].title().replace("And", "and")
+    continent = get_continent(country)
 
-    # Get average hostel price - will need to convert EUR to USD
-    sum_prices = 0
-    for hostel in hostels:
-        sum_prices += hostel[1]
-    avg_price = round(sum_prices / len(hostels))
+    # Generate HostelWorld URL based on random destination
+    url = format_url(city, country, continent)
+
+    # Scrape HostelWorld website to get hostel names, prices, and average price of first 10 hostels
+    soup = get_soup(url)
+    properties = get_hostels(soup)
+    hostels = get_hostel_names_and_prices(properties)
+    avg_price = get_price_average(hostels)
 
     return render_template('random.html', title='Random Destination', hostels=hostels, country=country, city=city,
                            avg_price=avg_price)
