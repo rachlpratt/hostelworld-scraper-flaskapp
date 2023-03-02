0e4adda68edabcd2fac7fc5051f16db3ee16ae5a
Rachel Pratt <rachelpratt@RachelsMac.local>
2023-03-02 12:56:10 -0500

    Update list of cities for destinations dropdown

--- app/cities.py
+++ app/cities.py
@@ -1,3 +1,7 @@
-cities = [("tokyo", "japan"), ("paris", "france"), ("rome", "italy"), ("sydney", "australia"), ("new york city", "usa"),
-          ("san francisco", "usa"), ("istanbul", "turkey"), ("athens", "greece"), ("dublin", "ireland"),
-          ("auckland", "new zealand"), ("vienna", "austria"), ("berlin", "germany"), ("madrid", "spain")]
+cities = [("paris", "france"), ("tokyo", "japan"), ("rome", "italy"), ("london", "england"), ("sydney", "australia"),
+          ("new york city", "usa"), ("lima", "peru"), ("toronto", "canada"), ("bangkok", "thailand"),
+          ("istanbul", "turkey"), ("athens", "greece"), ("madrid", "spain"), ("san francisco", "usa"),
+          ("dublin", "ireland"), ("mumbai", "india"), ("berlin", "germany"), ("budapest", "hungary"),
+          ("seoul", "south korea"), ("oslo", "norway"), ("ho chi minh", "vietnam"), ("havana", "cuba"),
+          ("shanghai", "china"), ("mexico city", "mexico"), ("buenos aires", "argentina"), ("marrakech", "morocco"),
+          ("cape town", "south africa"), ("auckland", "new zealand"), ("vienna", "austria"), ("cairo", "egypt")]
